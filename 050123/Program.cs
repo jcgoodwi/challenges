61fcb7e3fdcb94c4604d6517ba07286ca8251763
jacob <jacob.carter.goodwin@gmail.com>
2023-05-04 11:58:57 -0700

    challenge 050123

---
+++ 050123/Program.cs
@@ -0,0 +1,25 @@
+﻿public class goToOne
+{
+    public static int collatz(int n)
+    {
+        int counter = 0;
+        while (n > 1)
+        {
+            if (n % 2 == 0)
+            {
+                n = n / 2;
+            }
+            else
+            {
+                n = n * 3 + 1;
+            }
+            counter++;
+        }
+        return counter;
+    }
+    public static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(collatz(n));
+    }
+}
